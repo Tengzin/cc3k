0f85dd86e058777a77e75225b75db26288b0fc3d
kshitijng <kshitijngupta@gmail.com>
2017-07-22 12:15:34 -0400

    changes to randomgen

--- maplayout.cc
+++ maplayout.cc
@@ -2,8 +2,36 @@
 #include "tile.h"
 #include <sstream>
 using namespace std;
-
+//PC,stairway,potions,gold,enemies
 int PC_r,PC_c;
+int stair_r,stair_y;
+std::vector<Tile> r1;
+std::vector<Tile> r2;
+std::vector<Tile> r3;
+std::vector<Tile> r4;
+std::vector<Tile> r5;
+std::vector<vector<Tile> room;
+// Room 1 :
+// (4,4) (4,29)
+// (7,4) (7,29)
+// Room 2 : a
+// (4,40) (4,62)
+// (7,40) (7,63)
+// Room 2 : b //6
+// (8,61) (8,77)
+// (13,61) (13,77)
+// Room 3 : a
+// (17,66) (17,76)
+// (19,66) (17,76)
+// Room 3 : b //7
+// (20,38) (20,76)
+// (22,38) (22,76)
+// Room 4
+// (16,5) (16,25)
+// (22,5) (22,25)
+// Room 5
+// (11,39) (11,50)
+// (13,39) (13,50)
 
 int RandomNumber(n){
 	srand(time(0));
@@ -11,6 +39,16 @@ int RandomNumber(n){
 	randNum = 1+(rand()%n);
 	return randNum;
 }
+
+
+// void roords (int &room, int &x, int &y) {
+// 	room = RandomNumber(7);
+// 	Coords p = v[room-1];
+// 	int width = p.rx-p.lx;
+// 	int height = p.by-p.ty;
+// 	x = x + RandomNumber(width);
+// 	y = y + RandomNumber(height);
+// }
 
 mapLayout::init() {
   mapDisplay *md = new mapDisplay;
@@ -58,6 +96,11 @@ mapLayout::init() {
 		}
     layout.push_back(row);
   }
+	room.push_back(r1);
+	room.push_back(r2);
+	room.push_back(r3);
+	room.push_back(r4);
+	room.push_back(r5);
   for(int j = 0; j < 25; ++j) {
     for(int i = 0; i < 79; ++i) {
           // corners
@@ -105,22 +148,35 @@ ostream &operator<<(ostream &out, const mapLayout &md) {
   return out;
 }
 
-// mapLayout::placeInteractables() {
-//   //We first have to generate the stairs
-//   //First we select a room for the stairs
-//   //Generating a player character
-//   int n = RandomNumber(1) //Selecting a room.
-//   switch (n) {
-//     case 1:
-//     int w = r1.rx-r1.lx;
-//     int h = r1.ty-r1.by;
-//     int c1 = RandomNumber(w);
-//     int c2 = RandomNumber(h);
-//     int x = r1.lx + c1;
-//     int y = r1.ty + c2;
-//
-//   }
-// }
+void mapLayout::placeInteractables() {
+  //We first generate the PC
+	int char_n = RandomNumber(5);
+	int x = RandomNumber(room[char_n-1].size());
+	Info charloc = room[char_n-1][x-1].getInfo();
+	Info playerInfo = {0,0,,,}//Create struct for PC;
+	layout[charLoc.r][charLoc.c].change(playerInfo);
+	//DONE WITH PC
+	//Now stairs
+	int stair_n = RandomNumber(5);
+	while (stair_n == char_n) {
+		stair_n = RandomNumber(5);
+	}
+	x = RandomNumber(room[stair_n-1].size());
+	Info stairLoc = room[stair_n][x-1].getinfo();
+	stair_r = stairLoc.r;
+	stair_c = stairLoc.c;
+	//Done with Stairs (add stair location checker in move)
+	//Now make the potions.
+  for (int i = 0; i < potionLimit; ++i) {
+
+	}
+	for (int j = 0; j < goldLimit; ++j) {
+
+	}
+	for (int j = 0; j < EnemiesLimit; ++j) {
+
+	}
+}
 
 void mapLayout::move(string s) {
 	int x = 0;
